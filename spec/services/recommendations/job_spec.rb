7ad7670e0287cdbdcaa59de9a952e0af762068b4
niuage <niuage@gmail.com>
2016-12-09 16:10:46 -0500

    update the tests to make sure draft jobs are not recommended // uses factory girl

--- spec/services/recommendations/job_spec.rb
+++ spec/services/recommendations/job_spec.rb
@@ -2,10 +2,10 @@ require "rails_helper"
 
 describe Recommendations::Job do
   # job we want to find similar jobs for
-  let(:root_job) { Job.create(uid: 1337, dev_type: 2) }
+  let(:root_job) { create(:job, uid: 1337, dev_type: 2) }
 
   # user who just applied to root_job
-  let(:current_user) { User.create(uid: "1") }
+  let(:current_user) { create(:user, uid: "1") }
 
   subject { Recommendations::Job.new(root_job).for(current_user) }
 
@@ -19,24 +19,26 @@ describe Recommendations::Job do
         - with a role matching the given job's role
     } do
       # These 2 applied to the root job
-      applicant_1 = User.create(uid: "2")
-      applicant_2 = User.create(uid: "3")
+      applicant_1 = create(:user, uid: "2")
+      applicant_2 = create(:user, uid: "3")
 
       # this applicant applied to a recommended job, but not the root_job
-      random_applicant = User.create(uid: "4")
+      random_applicant = create(:user, uid: "4")
 
       # applicant_1, applicant_2 and random_applicant applied to it
-      first_job = Job.create(uid: 1, dev_type: 2)
+      first_job = create(:job, uid: 1, dev_type: 2)
       # applicant_1 and applicant_2 applied to it
-      second_job = Job.create(uid: 2, dev_type: 2)
+      second_job = create(:job, uid: 2, dev_type: 2)
       # only applicant_1 applied to it
-      third_job = Job.create(uid: 3, dev_type: 2)
+      third_job = create(:job, uid: 3, dev_type: 2)
       # not recommended because the current user already applied to it
-      excluded_job = Job.create(uid: 4, dev_type: 2)
+      excluded_job = create(:job, uid: 4, dev_type: 2)
       # should be obvious
-      lonely_job = Job.create(uid: 5, dev_type: 2)
+      lonely_job = create(:job, uid: 5, dev_type: 2)
       # wrong dev_type
-      wrong_dev_type_job = Job.create(uid: 6, dev_type: 3)
+      wrong_dev_type_job = create(:job, uid: 6, dev_type: 3)
+      # draft job
+      draft_job = create(:draft_job, uid: 7, dev_type: 2)
 
       # Setting up the APPLIED_TO relationships
       current_user.jobs << root_job
@@ -58,6 +60,7 @@ describe Recommendations::Job do
       third_job.applicants << applicant_1
 
       wrong_dev_type_job.applicants << applicant_1
+      draft_job.applicants << applicant_1
 
       # Finding the similar jobs
       similar_jobs = subject.similar_jobs(5)
